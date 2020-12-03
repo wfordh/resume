565db5c9e434251cbc51eff85f7769e63b9af7f4
Ford Higgins <wfordh@users.noreply.github.com>
2020-12-03 17:16:51 -0500

    add flavor ability to commit messages

--- run_resume_github.sh
+++ run_resume_github.sh
@@ -1,5 +1,8 @@
 #!/bin/zsh
 
+# Compiles the LaTeX and adds/pushes it to GitHub
+# You may pass a string as an argument for flavor text on the commit message
+
 echo "Rendering resume in LaTeX"
 xelatex fordhiggins_resume.tex
 echo "Adding to GitHub"
@@ -7,6 +10,6 @@ git add fordhiggins_resume.tex
 git add fordhiggins_resume.pdf
 echo "Committing to GitHub"
 now=`date`
-git commit -m "$now"
+git commit -m "$1 $now"
 echo "Pushing to GitHub"
 git push origin main
